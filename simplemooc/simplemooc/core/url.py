31bf43b67db2af609b4c04be264626133364b136
gleniooliveira <glenio.o.silva@gmail.com>
2016-08-25 20:32:16 -0300

    teste git

---
+++ simplemooc/simplemooc/core/url.py
@@ -0,0 +1,6 @@
+from django.conf.urls import include, url
+
+urlpatterns = [
+url(r'^$','simplemooc.core.views.home', name='home'),
+url(r'^contato/$','simplemooc.core.views.contact', name='contact'),
+]
